941c2b30934119065c278900ae026805f4c051ed
Premkalyan-Shannu <premkalyan.c@skyaigiants.com>
2026-09-04 16:50:30 +0530

    hello

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,9 @@ android {
     defaultConfig {
         applicationId = "com.prem.skudo"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
